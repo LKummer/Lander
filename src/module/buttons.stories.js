550bda1345453e782bbe9e85b95cb12740634759
Lior Kummer <LKummer41@gmail.com>
2020-02-24 20:17:34 +0200

    Add buttons module and stories.

---
+++ src/module/buttons.stories.js
@@ -0,0 +1,72 @@
+export default {
+  title: 'Buttons'
+}
+
+export const Buttons = function () {
+  return `
+  <ul class="buttons">
+    <li>
+      <button class="button button-primary">
+        First Button
+      </button>
+    </li>
+    <li>
+      <button class="button button-accent">
+        Second Button
+      </button>
+    </li>
+    <li>
+      <button class="button button-primary">
+        Third Button
+      </button>
+    </li>
+  </ul>
+  `
+}
+
+export const ManyButtons = function () {
+  return `
+  <ul class="buttons">
+    <li>
+      <button class="button button-primary">
+        First Button
+      </button>
+    </li>
+    <li>
+      <button class="button button-accent">
+        Second Button
+      </button>
+    </li>
+    <li>
+      <button class="button button-primary">
+        Third Button
+      </button>
+    </li>
+    <li>
+      <button class="button button-accent">
+        Fourth Button
+      </button>
+    </li>
+    <li>
+      <button class="button button-primary">
+        Fifth Button
+      </button>
+    </li>
+    <li>
+      <button class="button button-accent">
+        Sixth Button
+      </button>
+    </li>
+    <li>
+      <button class="button button-primary">
+        Seventh Button
+      </button>
+    </li>
+    <li>
+      <button class="button button-accent">
+        Eighth Button
+      </button>
+    </li>
+  </ul>
+  `
+}
