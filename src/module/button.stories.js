ea826c0ead9dff5a1354366e2a5c61f59792d4a2
Lior Kummer <LKummer41@gmail.com>
2020-02-24 19:39:50 +0200

    Add file and story for the button module.

---
+++ src/module/button.stories.js
@@ -0,0 +1,19 @@
+export default {
+  title: 'Button'
+}
+
+export const Semantic = function () {
+  return `
+  <button class="button">
+    Semantic Button
+  </button>
+  `
+}
+
+export const Anchor = function () {
+  return `
+  <a href class="button">
+    Link Button
+  </a>
+  `
+}
