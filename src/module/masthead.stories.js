dd11bb5798de2aee5901508f25e94d5eac56d2d7
Lior Kummer <LKummer41@gmail.com>
2020-02-25 00:32:26 +0200

    Add masthead module with a story.

---
+++ src/module/masthead.stories.js
@@ -0,0 +1,34 @@
+export default {
+  title: 'Masthead',
+  parameters: {
+    componentSubtitle: 'Main introduction of visitors to the site.'
+  }
+}
+
+export const Normal = function () {
+  return `
+  <section class="masthead">
+    <h1 class="masthead-heading">
+      This is the masthead title.
+    </h1>
+    <h2 class="masthead-subheading">
+      This is the masthead's subtitle, which will contain a little bit more
+      text.
+    </h2>
+    <nav>
+      <ul class="buttons">
+        <li>
+          <a href class="buttons-button button button-primary">
+            First Button
+          </a>
+        </li>
+        <li>
+          <a href class="buttons-button button button-accent">
+            Second Longer Button
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </section>
+  `
+}
